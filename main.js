92fb7529bccf3597e026104662f5140d480cd47a
S0KL0 <s0kl0.dev@gmail.com>
2025-06-13 12:34:18 +0300

    implemented electron window

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+// https://www.youtube.com/watch?v=TkAiVKfWtjI
+
+let win
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 1000,
+        height: 800
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
